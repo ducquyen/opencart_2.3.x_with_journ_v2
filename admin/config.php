8a7fa140cc2f108060b9f427accb5f31ba847bff
weblips <weblips@gmail.com>
2016-09-12 11:08:02 +0300

    install

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://savstore.com/admin/');
+define('HTTP_CATALOG', 'http://savstore.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://savstore.com/admin/');
+define('HTTPS_CATALOG', 'http://savstore.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/alex/sites/savstore.com/admin/');
+define('DIR_SYSTEM', '/home/alex/sites/savstore.com/system/');
+define('DIR_IMAGE', '/home/alex/sites/savstore.com/image/');
+define('DIR_LANGUAGE', '/home/alex/sites/savstore.com/admin/language/');
+define('DIR_TEMPLATE', '/home/alex/sites/savstore.com/admin/view/template/');
+define('DIR_CONFIG', '/home/alex/sites/savstore.com/system/config/');
+define('DIR_CACHE', '/home/alex/sites/savstore.com/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/alex/sites/savstore.com/system/storage/download/');
+define('DIR_LOGS', '/home/alex/sites/savstore.com/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/alex/sites/savstore.com/system/storage/modification/');
+define('DIR_UPLOAD', '/home/alex/sites/savstore.com/system/storage/upload/');
+define('DIR_CATALOG', '/home/alex/sites/savstore.com/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'falcons');
+define('DB_DATABASE', 'savstore_ua');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocs_');
